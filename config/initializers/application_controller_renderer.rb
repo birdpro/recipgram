573cb88ca0d6d09696fab1518a52f08193b79e0c
torikun <sc115039@ibe.kagoshima-u.ac.jp>
2020-08-11 00:15:31 +0900

    part2実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5d803b3975274b152fcd0476e384d290fe9245fd571e83754642b9471cbf8640e5a17ae97dcd6b07f29bac46decce378e5b6fc8344d1866b8189f1ac08191490'
